9f5be7b559fad584084bc24350760b468d414d11
stealthman22 <tdotonline@gmail.com>
2020-12-16 02:36:12 +0000

    finished setting up react router, login and register routes working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@
 import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-
 // Components
 import {Navbar} from "./components/layouts/Navbar";
 import {Landing} from "./components/layouts/Landing";
+import { Register} from "./components/auth/Register";
+import { Login} from "./components/auth/Login";
 import './App.css';
 
-function App() {
-  return (
-    <Fragment >
-    <Navbar />
-    <Landing />
-    </Fragment>
+const  App = () => {
+ return(
+   <Router>
+      <Fragment >
+       <Navbar />
+       {/* creating the homepage */}
+       <Route exact path="/" component={Landing} />
+       <section className="container">
+         {/* other frontend routes */}
+         <Switch>
+         <Route exact path="/register" component={Register}/>
+        <Route exact path="/login" component={Login} />
+         </Switch>
+       </section>
+      
+     </Fragment>
+    </Router>
   );
 }
 
